b78e8196c2a44599f31504ad876253c2fb3fd16a
VladimirBdoyan <86346545+VladimirBdoyan@users.noreply.github.com>
2021-11-19 02:52:00 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,4 +26,36 @@ public class Main {
         double pointsDis = point.distance(secondPoint);
         System.out.println(" Distance of between those two points is " + pointsDis);
     }
+    
+    public class Point {
+
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance() {
+        double dis;
+        dis = Math.sqrt((x * x) + (y * y));
+        return dis;
+    }
+
+    public double distance(Point secondPoint) {
+        double dis;
+        dis = Math.sqrt((this.x - secondPoint.getX()) * (this.x - secondPoint.getX()) +
+                (this.y - secondPoint.getY()) * (this.y - secondPoint.getY()));
+        return dis;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
 }
